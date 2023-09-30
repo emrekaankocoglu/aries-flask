c15fff7d4c09f91f8c23badbaed1286d6320d936
emrekaankocoglu <emrekaankocoglu@gmail.com>
2023-09-30 14:21:44 +0000

    added feedback and success

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request,render_template
 from tinydb import TinyDB, Query
 db = TinyDB('db.json')
+db_feedback = db.table('feedback')
+db_contact = db.table('contact')
 
 
 app = Flask(__name__,
@@ -11,9 +13,10 @@ app = Flask(__name__,
 @app.route('/contact.html', methods=['GET','POST'])
 def contact():
     if request.method == 'POST':
-        print(request.form)
-        db.insert(request.form)
-        return render_template("contact.html", message="Thank you for your message", message_type="success")
+        formdata = dict(request.form)
+        formdata.update({"read":False})
+        db_contact.insert(formdata)
+        return render_template("success.html", message="Thank you for your message", message_type="success")
     else:
         return render_template("contact.html")
 @app.route('/index.html', methods=['GET'])
@@ -23,6 +26,16 @@ def index():
 @app.route('/', methods=['GET'])
 def ins():
     return render_template("index.html")
+
+@app.route('/fb', methods=['GET','POST'])
+def feedback():
+    if request.method == 'POST':
+        formdata = dict(request.form)
+        formdata.update({"read":False, "token" : request.args.get('t')})
+        db_feedback.insert(formdata)
+        return render_template("success.html", message="Thank you for your message", message_type="success")
+    else:
+        return render_template("feedback.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
